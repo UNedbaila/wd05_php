c4ed84222802571af98e715cb861a5be432a2444
Uladzimir Nedbaila <103594123+UNedbaila@users.noreply.github.com>
2022-08-07 19:24:34 +0300

    Update Driver.php
    Correction

--- src/class/Driver.php
+++ src/class/Driver.php
@@ -8,4 +8,25 @@ class Driver extends Worker5
 {
     private $experience;
     private $category;
+    
+    public function __construct($experience, $category) {
+			$this->experience = $experience;
+			$this->category = $category;
+		}
+
+		public function setExperience() {
+			$this->experience = $experience;
+		}
+
+		public function setCategory() {
+			$this->category = $category;
+		}
+
+		public function getExperience() {
+			return $this->experience;
+		}
+
+		public function getCategory() {
+			return $this->category;
+		}
 }
